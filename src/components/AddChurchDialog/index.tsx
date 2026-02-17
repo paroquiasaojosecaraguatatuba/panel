93c85cae57ddc1c7ea34a1a79385254919303056
Giselle Hoekveld <gisellehoekveld@gmail.com>
2026-02-17 10:13:19 -0300

    feat: add Button component and integrate it into AddChurchDialog; remove unused Input component

--- src/components/AddChurchDialog/index.tsx
+++ src/components/AddChurchDialog/index.tsx
@@ -6,6 +6,7 @@ import * as FileInput from "@/components/Form/FileInput";
 import { AddCard } from "../AddCard";
 
 import "./styles.css";
+import Button from "../Button";
 
 export const AddChurchDialog = () => {
   return (
@@ -36,22 +37,18 @@ export const AddChurchDialog = () => {
           </div>
 
           <div className="p-4 flex gap-3 justify-end items-center border-t border-zinc-200">
-            <Dialog.Close
-              type="button"
-              className="cursor-pointer rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-brand-0 hover:border-brand-300 outline-none focus-visible:ring-4 focus-visible:ring-brand-100 focus-visible:ring-offset-1"
-            >
-              Cancelar
+            <Dialog.Close asChild>
+              <Button variant="outline">Cancelar</Button>
             </Dialog.Close>
-            <Dialog.Close
-              type="submit"
-              className="cursor-pointer rounded-lg bg-brand-800 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-brand-900 outline-none focus-visible:ring-4 focus-visible:ring-brand-100 focus-visible:ring-offset-1"
-            >
-              Salvar Alterações
+            <Dialog.Close asChild>
+              <Button>Salvar Alterações</Button>
             </Dialog.Close>
           </div>
 
-          <Dialog.Close className="group rounded-full w-7 h-7 absolute top-4 right-4 bg-gray-100 flex items-center justify-center text-gray-500 hover:bg-brand-0 hover:ring-1 hover:ring-brand-300 cursor-pointer transition-colors outline-none focus-visible:ring-4 focus-visible:ring-brand-100 focus-visible:ring-offset-1">
-            <CloseIcon className="w-4 h-4 text-zinc-400 group-hover:text-zinc-500" />
+          <Dialog.Close asChild className="absolute top-4 right-4">
+            <Button variant="ghost">
+              <CloseIcon className="w-4 h-4" />
+            </Button>
           </Dialog.Close>
         </Dialog.Content>
       </Dialog.Portal>
---
+++ src/components/Button.tsx
@@ -0,0 +1,32 @@
+import type { ComponentProps } from "react";
+import { tv, type VariantProps } from "tailwind-variants";
+
+const button = tv({
+  base: [
+    "cursor-pointer rounded-lg  px-4 py-2 text-sm font-semibold outline-none shadow-sm",
+    "focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-violet-500",
+    "active:opacity-80",
+  ],
+
+  variants: {
+    variant: {
+      primary:
+        "bg-brand-800 text-white hover:bg-brand-900 focus-visible:ring-4 focus-visible:ring-brand-100 focus-visible:ring-offset-1",
+      ghost:
+        "rounded-md px-2 hover:bg-brand-50 text-zinc-500 shadow-none hover:text-brand-600 focus-visible:ring-4 focus-visible:ring-brand-100 focus-visible:ring-offset-1",
+      outline:
+        "border border-zinc-300 text-zinc-700 hover:bg-brand-0 hover:border-brand-300 focus-visible:ring-4 focus-visible:ring-brand-100 focus-visible:ring-offset-1",
+    },
+  },
+
+  defaultVariants: {
+    variant: "primary",
+  },
+});
+
+export type ButtonProps = ComponentProps<"button"> &
+  VariantProps<typeof button>;
+
+export default function Button({ variant, className, ...props }: ButtonProps) {
+  return <button {...props} className={button({ variant, className })} />;
+}
