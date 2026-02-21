4e543aeeeb44488dc912abc835890b45b57e205c
Giselle Hoekveld <gisellehoekveld@gmail.com>
2026-02-21 16:24:00 -0300

    fix: remove unused imports and simplify PrivateLayout component structure

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,37 +1,17 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Sidebar } from "@/components/Sidebar";
 import "../globals.css";
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Paróquia São José - Caraguatatuba",
-  description:
-    "Site oficial da Paróquia São José em Caraguatatuba, SP. Encontre informações sobre missas, eventos, ministérios e serviços comunitários. Junte-se a nós na fé e na celebração da vida cristã.",
-};
-
-export default function RootLayout({
+export default function PrivateLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className="antialiased bg-brand-0">
-      <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-      </head>
-      <body className={`${inter.variable} antialiased`}>
-        <div className="lg:grid-cols-app min-h-screen lg:grid">
-          <Sidebar />
-          <main className="max-w-[100vw] px-4 pt-30 pb-12 lg:col-start-2 lg:px-8 lg:pt-8">
-            {children}
-          </main>
-        </div>
-      </body>
-    </html>
+    <div className="lg:grid-cols-app min-h-screen lg:grid">
+      <Sidebar />
+      <main className="max-w-[100vw] px-4 pt-30 pb-12 lg:col-start-2 lg:px-8 lg:pt-8">
+        {children}
+      </main>
+    </div>
   );
 }
